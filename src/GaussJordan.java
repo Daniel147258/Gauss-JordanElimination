c2f3c818bf35350c98f9bd3d3f0f3ecb74db1bdb
Daniel Dziaba <danieldziaba@gmail.com>
2024-03-01 19:29:28 +0100

    OK

--- src/GaussJordan.java
+++ src/GaussJordan.java
@@ -20,9 +20,7 @@ public class GaussJordan {
 
     public void GaussJordan ()
     {
-        // (i) je stale definovany pre riadok matice
-        // (j) je stale definovany pre stlpec matice
-
+        // Cesta dole
         for (int i = 0; i < this.pocetRiadkov; i++){
             int pokracuj = 0;
             boolean nulovyRiadok = true;
@@ -38,10 +36,13 @@ public class GaussJordan {
             if(nulovyRiadok){
                 this.indexPivota[i] = 0;
             }
+
+            // Kontrola ci ma riesenie
             if (this.indexPivota[i] == this.pocetStlpcov - 1){
                 this.maRiesenie = false;
                 break;
             }
+
             double pivot = this.matica[i][this.indexPivota[i]];
             if (pivot != 1 && pivot != 0) {
                 for (int j = 0; j < this.pocetStlpcov; j++){
@@ -55,12 +56,50 @@ public class GaussJordan {
                     this.matica[pokracuj][j] += this.matica[i][j] * nasobitel;
                 }
             }
+
         }
+
+        //Cesta hore
+        for (int i = this.pocetRiadkov - 1; i > 0; --i){
+            boolean maRiadokPivot = false;
+            double pivot = 0;
+            int indexPivota = 0;
+
+            //Najdem pivot
+            for (int j = 0; j < this.pocetStlpcov - 1; ++j){
+                if(this.matica[i][j] != 0){
+                    pivot = this.matica[i][j];
+                    maRiadokPivot = true;
+                    indexPivota = j;
+                    break;
+                }
+            }
+
+            if(maRiadokPivot) {
+                // Vynulovanie nad pivotom
+                for (int a = i - 1; a > -1 ; --a){ // riadky
+                    double nasobitel = 0;
+                    for (int j = indexPivota; j < this.pocetStlpcov; ++j){ // stlpce
+                        if(j == indexPivota) {
+                            nasobitel = -this.matica[a][j];
+                            this.matica[a][j] += (pivot * nasobitel);
+                        }
+                        else{
+                            if(this.matica[a][j] != 0) {
+                                this.matica[a][j] += (this.matica[a + 1][j] * nasobitel);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
         System.out.println("Matica po uprave:");
         this.VypisMaticu();
-        System.out.print("Riesenie matice je nasledovne:");
+        System.out.print("Riešenie matice je následovne: ");
         this.vyhodnotRiesenie();
     }
+
     private int pocetNenulovychRiadkov(){
         int nenulove = 0;
         for(int i = 0; i < this.pocetRiadkov; i++){
@@ -73,19 +112,26 @@ public class GaussJordan {
         }
         return nenulove;
     }
+
     private void vyhodnotRiesenie(){
         int pocetPremennych = this.pocetStlpcov - 1;
         if(maRiesenie){
             int a = pocetPremennych - this.pocetNenulovychRiadkov();
             if(a == 0){
-                System.out.println("Matica ma prave jedno riesenie");
+                System.out.println("Matica ma práve jedno riešenie");
+                String b = "";
+                for(int i = 0; i < this.pocetRiadkov; i++){
+                    b += "x" + (i + 1) + " = " + String.format("%.2f",this.matica[i][this.pocetStlpcov - 1]) + "\n";
+                }
+                System.out.println(b);
             }
             if(a > 0 ){
-                System.out.println("Matica ma nekonecne vela rieseni");
+                System.out.println("Matica ma nekonečne veľa riešení");
+
             }
         }
         else {
-            System.out.println("Matica nema riesenie ");
+            System.out.println("Matica nemá riešenie ");
         }
     }
 
@@ -95,7 +141,7 @@ public class GaussJordan {
             for(int j = 0; j < this.pocetStlpcov; j++){
                 a += String.format("%7.2f", this.matica[i][j]);
                 if(j == this.pocetStlpcov - 2){
-                    a += "|";
+                    a += "  |";
                 }
             }
             a += "\n";
--- src/main.java
+++ src/main.java
@@ -3,7 +3,7 @@ import java.io.FileNotFoundException;
 public class main {
     public static void main(String args[]) throws FileNotFoundException {
         GaussJordan g =  new GaussJordan(2,5);
-        g.nacitajMaticuZoSuboru("C:\\Users\\danie\\IdeaProjects\\GaussJordan elimination\\src\\Priklad1.txt");
+        g.nacitajMaticuZoSuboru("C:\\Users\\Daniel\\IdeaProjects\\Gauss-JordanElimination\\src\\NekonecneVela.txt");
         g.GaussJordan();
     }
 }
